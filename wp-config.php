888be0a7d658887e41b7d8238208db8f5b4ea44d
kyhfan <yh.kim@minivertising.kr>
2014-09-23 11:22:40 +0900

    DB Setting change

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,14 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+//define('DB_NAME', 'minivertising');
 define('DB_NAME', 'minivertising');
 
 /** MySQL database username */
-define('DB_USER', 'minivertising');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'alslqj~1');
+define('DB_PASSWORD', 'apmsetup');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
